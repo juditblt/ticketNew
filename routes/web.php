6797c37b03f77e4ed6b150967cb4297ef660fb25
juditblt <nagymiklosi.judit@blathy.info>
2022-05-02 21:35:55 +0200

    Changes...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PrivateController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PublicController::class, 'index']);
+/*
+Route::get('/home', function (){
+    return "HOME";
+});
+*/
+Route::get('/home', [PrivateController::class, 'home'])->name('private.home')
+    ->middleware('auth');
+Route::post('/store', [PrivateController::class, 'store'])->name('private.ticket.store')
+    ->middleware('auth');
+Route::get('/admin/categories', [CategoryController::class, 'index'])->name('admin.categories')
+    ->middleware('auth');
+Route::post('admin/categories/store', [CategoryController::class, 'store'])->name('admin.categories.store')
+    ->middleware('auth');
+Route::post('admin/categories/destroy/{id}', [CategoryController::class, 'destroy'])->name('admin.categories.destroy')
+    ->middleware('auth');
+Route::post('admin/categories/enable', [CategoryController::class, 'enable'])->name('admin.categories.enable')
+    ->middleware('auth');
+Route::post('admin/categories/disable', [CategoryController::class, 'disable'])->name('admin.categories.disable')
+    ->middleware('auth');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
